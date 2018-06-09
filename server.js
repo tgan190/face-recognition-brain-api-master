f26e49dc984748288207cbb04cf51351444aa2e5
James Gan <teowgan@gmail.com>
2018-06-09 23:56:56 +1000

    set connectionString

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const PORT = process.env.PORT || 5000;
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'xxx',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
